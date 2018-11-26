bfd6c5b83f00e873a22957bbf708359bf0fdbbf8
Javier <Javier@10.101.116.142>
2018-11-26 11:32:25 +0100

    Inicio

---
+++ Demos/src/application/Main.java
@@ -0,0 +1,46 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Button btn = new Button("Saluda");
+			Label rslt = new Label("Hola");
+			rslt.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+			TextField txt = new TextField("Mundo");
+			btn.setOnAction(e -> { rslt.setText("Hola " + txt.getText()); });
+			VBox root = new VBox();
+			root.getChildren().addAll(txt, btn, rslt);
+			root.setSpacing(8);
+			root.setPadding(new Insets(10));
+			primaryStage.setScene(new Scene(root,400,400));
+
+//			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+			primaryStage.setTitle("Demos del curso");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
